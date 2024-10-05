b1c25bb23ca7c864cfe1c5d94d4c3df2e90a7557
Samuel Medeiros de Albuquerque <84237653+SamuelMedeirosA@users.noreply.github.com>
2024-10-05 20:22:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 // script.js
 
 // Função para criar uma bolha
+const photos = ['/img/G.jpg', '/img/NI.jpg', '/img/S.jpg', '/img/T.jpg', '/img/J.jpg', '/img/N.jpg', '/img/JP.jpg'];
+let photoIndex = 0;
+
 function createBubble() {
     const bubble = document.createElement('div');
     bubble.classList.add('bubble');
+
+    // Define se a bolha vai ter uma foto (50% das bolhas)
+    const hasPhoto = Math.random() > 0.6;
+
+    if (hasPhoto) {
+        bubble.classList.add('photo');
+        bubble.style.backgroundImage = `url(${photos[photoIndex]})`;
+        bubble.style.backgroundSize = 'cover';
+        bubble.style.backgroundPosition = 'center';
+        photoIndex = (photoIndex + 1) % photos.length; // Intercalar entre as fotos
+    } else {
+        bubble.style.background = 'rgba(255, 255, 255, 0.5)'; // Transparência para bolhas sem fotos
+    }
 
     // Tamanho da bolha aleatório
     const size = Math.random() * 60 + 20; // Tamanho entre 20px e 80px
@@ -17,32 +33,29 @@ function createBubble() {
     const duration = Math.random() * 5 + 5; // Duração entre 5s e 10s
     bubble.style.animationDuration = `${duration}s`;
 
-    // Adiciona a função de clique para todas as bolhas
-    bubble.addEventListener('click', handleBubbleClick);
+    // Adiciona a função de estouro ao passar o mouse
+    bubble.addEventListener('mouseover', () => {
+        bubble.classList.add('burst');
+        setTimeout(() => {
+            bubble.remove();
+        }, 100); // Tempo para o estouro ocorrer
+    });
 
     // Adiciona a bolha ao body
     document.body.appendChild(bubble);
 
-    // Remove a bolha após a animação
+    // Remove a bolha automaticamente após a animação, caso não seja estourada
     setTimeout(() => {
-        bubble.remove();
-    }, duration * 1000);
+        if (!bubble.classList.contains('burst')) {
+            bubble.remove();
+        }
+    }, duration * 700);
 }
 
-// Função para tratar o clique em qualquer bolha
-function handleBubbleClick() {
-    // Adiciona um efeito ao clicar na bolha
-    this.style.transform = 'scale(1.2)';
-    this.style.transition = 'transform 0.3s ease-in-out';
 
-    // Remove a bolha após a animação
-    setTimeout(() => {
-        window.location.href = 'home.html'; // Redireciona para a próxima página
-    }, 300); // Atraso para mostrar o efeito
-}
 
 // Criar bolhas a cada intervalo
-setInterval(createBubble, 500);
+setInterval(createBubble, 300);
 
 // Função para tratar o clique nas bolhas e aplicar a transição suave
 function handleBubbleClick() {
